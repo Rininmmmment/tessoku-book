b322dc98d18b7be243f31e8d3b5712fc69aea5c5
Rininmmmment <rinr10693otes@gmail.com>
2023-02-06 15:22:50 +0900

    20220206_15:22

--- a46/a46.py
+++ a46/a46.py
@@ -1,10 +1,4 @@
-# (x, y)
-
-N = int(input())
-X, Y = [None] * (N-1), [None] * (N-1)
-for i in range(N-1):
-    X[i], Y[i] = map(int, input().split())
-
+# 二次元の点を扱うクラス
 class point2d:
 	def __init__(self, x, y):
 		self.x = x
@@ -13,4 +7,41 @@ class point2d:
 	def dist(self, p):
 		return ((self.x - p.x) ** 2 + (self.y - p.y) ** 2) ** 0.5
 
-print(point2d(X[0], Y[0]).dist(point2d(X[1], Y[1])))
+# 貪欲法によって答えを求める関数
+# （ここでは都市の番号を 0-indexed で扱っていることに注意）
+def play_greedy(n, points):
+	# 配列などの初期化
+	current_place = 0
+	visited = [ False ] * n
+	visited[0] = True
+	P = [ 0 ]
+	# 貪欲法スタート
+	for i in range(1, n):
+		mindist = 10 ** 10 # 現時点での距離の最小
+		min_id = -1 # 次はどの都市に移動すればよいか
+		# 距離が最小となる都市を探す
+		for j in range(n):
+			if not visited[j] and mindist > points[current_place].dist(points[j]):
+				mindist = points[current_place].dist(points[j])
+				min_id = j
+		# 現在位置の更新
+		visited[min_id] = True
+		P.append(min_id)
+		current_place = min_id
+	# 最後に訪問する都市
+	P.append(0)
+	return P
+
+# 入力
+N = int(input())
+points = [ None ] * N
+for i in range(N):
+	x, y = map(int, input().split())
+	points[i] = point2d(x, y)
+
+# 貪欲法
+answer = play_greedy(N, points)
+
+# 答えを出力（配列 answer の要素は 0-indexed になっていることに注意）
+for i in answer:
+	print(i + 1)
